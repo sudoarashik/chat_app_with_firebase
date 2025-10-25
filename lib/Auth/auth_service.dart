c5f9fb3da0ed6df526625219f5f869502a065350
arashikfl <arashik2197@gmail.com>
2025-10-25 12:55:43 +0600

    I have successfully implemented Register and Sign In method

--- lib/Auth/auth_service.dart
+++ lib/Auth/auth_service.dart
@@ -1,37 +1,37 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-
 class AuthService {
-
-  //instance of auth
+  // Instance of FirebaseAuth
   final FirebaseAuth auth = FirebaseAuth.instance;
-  Future<UserCredential> signInWithEmailPassword(String email, password) async {
-    try { UserCredential userCredential = await auth.signInWithEmailAndPassword(email: email, password:password,);
-    return userCredential;
-    } on FirebaseAuthException catch (e) {throw Exception(e.code);}
+
+  // Sign in
+  Future<UserCredential> signInWithEmailPassword(String email, String password) async {
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
   }
 
+  // Sign up
+  Future<UserCredential> signUpWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
 
-
-  //sign in
-
-  //sign up
-
-  //sign out
-   Future<void> signOut() async{
-    return await auth.signOut();
-   }
-  //errors
-
-
-
-
-
-
-
-
-
-
-
-
+  // Sign out
+  Future<void> signOut() async {
+    await auth.signOut();
+  }
 }
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,114 +1,124 @@
+import 'package:chat_app_with_firebase/Auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
 import '../components/my_button.dart';
 import '../components/my_textfield.dart';
 
 class RegisterPage extends StatelessWidget {
-
   // Email & password text editing controllers
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _pwController = TextEditingController();
   final TextEditingController _confirmPwController = TextEditingController();
 
   // Tap to go Login page
-
   final void Function()? onTap;
 
+  RegisterPage({super.key, required this.onTap});
 
-  RegisterPage({super.key, required this.onTap });
+  // Register Method
+  void register(BuildContext context) {
+    final auth = AuthService();
 
-  //Register Method
-  void register() {}
-
-
+    if (_pwController.text == _confirmPwController.text) {
+      try {
+        auth.signUpWithEmailAndPassword(
+            _emailController.text, _pwController.text);
+      } catch (e) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text(e.toString()),
+          ),
+        );
+      }
+    } else {
+      // password don't match => tell the user to fix
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text("Passwords don't match"),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return
-      Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        body: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Logo
-                const Icon(
-                  Icons.message,
-                  size: 60,
-                ),
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Logo
+              const Icon(Icons.message, size: 60),
+              const SizedBox(height: 50),
 
-                const SizedBox(height: 50),
+              // Create Account
+              const Text(
+                "Let's Create an Account for You",
+                style: TextStyle(color: Colors.black, fontSize: 16),
+              ),
+              const SizedBox(height: 25),
 
-                // Create Account
-                const Text(
-                  "Let's Create an Account for You",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
+              // Email text field
+              MyTextfield(
+                hintText: 'Email',
+                obscuretext: false,
+                controller: _emailController,
+              ),
+              const SizedBox(height: 10),
+
+              // Password text field
+              MyTextfield(
+                hintText: 'Password',
+                obscuretext: true,
+                controller: _pwController,
+              ),
+              const SizedBox(height: 10),
+
+              // Confirm Password text field
+              MyTextfield(
+                hintText: 'Confirm Password',
+                obscuretext: true,
+                controller: _confirmPwController,
+              ),
+              const SizedBox(height: 25),
+
+              // Register button
+              MyButton(
+                text: "Register",
+                onTap: () => register(context),
+              ),
+              const SizedBox(height: 25),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already Have an Account?",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
                   ),
-                ),
-
-                const SizedBox(height: 25),
-
-                // Email text field
-                MyTextfield(
-                  hintText: 'Email',
-                  obscuretext: false,
-                  controller: _emailController,
-                ),
-
-                const SizedBox(height: 10),
-
-                // Password text field
-                MyTextfield(
-                  hintText: 'Password',
-                  obscuretext: true,
-                  controller: _pwController,
-                ),
-
-                const SizedBox(height: 10),
-
-                //Confirm Password text field
-                MyTextfield(
-                  hintText: 'Confirm Password',
-                  obscuretext: true,
-                  controller: _confirmPwController,
-                ),
-
-
-
-                const SizedBox(height: 25),
-
-                // Login button
-                MyButton(
-                  text: "Register",
-                  onTap: register,
-                ),
-
-                const SizedBox(height: 25),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Already Have an Account?",
-                      style: TextStyle(color: Theme.of(context).colorScheme.primary),),
-
-                    GestureDetector(
-                      onTap: onTap,
-                      child: Text("Login Now",
-                        style: TextStyle(fontWeight: FontWeight.bold,
-                            color: Theme.of(context).colorScheme.primary
-                        ),),
-                    )
-                  ],
-                )
-
-                // Register now text
-              ],
-            ),
+                  const SizedBox(width: 5),
+                  GestureDetector(
+                    onTap: onTap,
+                    child: Text(
+                      "Login Now",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
-      );
+      ),
+    );
   }
 }
